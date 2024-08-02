bcb45a5d31d5ab20dea25ad6b51063295c4ce351
fahmid99 <fahmidahmed024@gmail.com>
2024-08-02 16:41:42 +1000

    3.9 to 3.11 Full Stack Phonebook

--- part1/countries/src/services/countries.jsx
+++ part1/countries/src/services/countries.jsx
@@ -9,7 +9,7 @@ const getAll = () => {
 
 const getWeather = (lat, lon, api_key) => {
   return axios.get(
-    `https://api.openweathermap.org/data/2.0/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`
   );
 };
 
